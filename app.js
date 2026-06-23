c5d1698ae2f1b3e2711eef2304f7a39a1394fd2f
skuzzy <skuzzytopten@gmail.com>
2026-06-23 09:46:07 +0700

    Day 2 - Route and Parameters

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const app = express();
 
 const port = 3000;
 
+// route
+app.get('/', (req, res) => {
+    res.send("Welcome to Express.js");
+});
+
+// route parameters
+app.get("/user/:name", (req, res) => {
+    res.send(`Hello ${req.params.name}`);
+});
+
+// query parameters
+app.get("/search", (req, res) => {
+    const keyword = req.query.keyword;
+    res.send(`Searching for: ${keyword}`);
+});
+
 // run express server
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
